e592d208b949ed38c8372e7e557fe60deb5c4e93
ashleynguyen07 <nguyentramy.se@gmail.com>
2023-04-21 15:51:30 +0700

    delete

--- COURSERA/PE SUMMER 2021/Resource/js/mycode.js
+++
@@ -1,9 +0,0 @@
-function myCode(){
-    var v1 = document.getElementById("fullname").value;
-    var v2 = document.getElementById("phone").value;
-    var v3 = document.getElementById("email").value;
-
-    if(v1=="" || v2=="" || v3==""){
-        alert("Please fill all field");
-    }
-}
--- COURSERA/[Nguyên mẫu] Đề tai nghe/js/myCode.js
+++
@@ -1,4 +0,0 @@
-function showContact(){
-    var phoneNumber = prompt("Xin chào, mời nhập vào số điện thoại");
-    alert("Chúng tôi sẽ gọi cho bạn qua số điện thoại " + phoneNumber)
-}
